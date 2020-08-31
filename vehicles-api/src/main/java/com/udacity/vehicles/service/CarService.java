8d365f8ed033b2e72b3c4bb11a89f0c55833938a
ozlop <oswaldo.t.lopez@gmail.com>
2020-08-31 16:47:56 -0700

    Add Car Service implementation
    The missing CRUD methods were implemnted for the Car service layer. The
    JpaRepository methods were used used for in tying each of the methods to
    the persistence layer.
    
    Where applicable, the  methods throw the CarNotFoundException exception
    when a vehicle id that is non-existent in the applicatin data is
    provided.

--- vehicles-api/src/main/java/com/udacity/vehicles/service/CarService.java
+++ vehicles-api/src/main/java/com/udacity/vehicles/service/CarService.java
@@ -1,8 +1,11 @@
 package com.udacity.vehicles.service;
 
+import com.udacity.vehicles.client.maps.MapsClient;
+import com.udacity.vehicles.client.prices.PriceClient;
 import com.udacity.vehicles.domain.car.Car;
 import com.udacity.vehicles.domain.car.CarRepository;
 import java.util.List;
+
 import org.springframework.stereotype.Service;
 
 /**
@@ -15,12 +18,14 @@ public class CarService {
 
     private final CarRepository repository;
 
-    public CarService(CarRepository repository) {
-        /**
-         * TODO: Add the Maps and Pricing Web Clients you create
-         *   in `VehiclesApiApplication` as arguments and set them here.
-         */
+    private final PriceClient priceClient;
+
+    private final MapsClient mapsClient;
+
+    public CarService(CarRepository repository, PriceClient priceClient, MapsClient mapsClient) {
         this.repository = repository;
+        this.priceClient = priceClient;
+        this.mapsClient = mapsClient;
     }
 
     /**
@@ -37,33 +42,16 @@ public class CarService {
      * @return the requested car's information, including location and price
      */
     public Car findById(Long id) {
-        /**
-         * TODO: Find the car by ID from the `repository` if it exists.
-         *   If it does not exist, throw a CarNotFoundException
-         *   Remove the below code as part of your implementation.
-         */
-        Car car = new Car();
+        if (id != null) {
+            return repository.findById(id)
+                    .map(foundCar -> {
+                        foundCar.setPrice(priceClient.getPrice(id));
+                        foundCar.setLocation(mapsClient.getAddress(foundCar.getLocation()));
+                        return foundCar;
+                    }).orElseThrow(CarNotFoundException::new);
+        }
 
-        /**
-         * TODO: Use the Pricing Web client you create in `VehiclesApiApplication`
-         *   to get the price based on the `id` input'
-         * TODO: Set the price of the car
-         * Note: The car class file uses @transient, meaning you will need to call
-         *   the pricing service each time to get the price.
-         */
-
-
-        /**
-         * TODO: Use the Maps Web client you create in `VehiclesApiApplication`
-         *   to get the address for the vehicle. You should access the location
-         *   from the car object and feed it to the Maps service.
-         * TODO: Set the location of the vehicle, including the address information
-         * Note: The Location class file also uses @transient for the address,
-         * meaning the Maps service needs to be called each time for the address.
-         */
-
-
-        return car;
+        return null;
     }
 
     /**
@@ -89,16 +77,14 @@ public class CarService {
      * @param id the ID number of the car to delete
      */
     public void delete(Long id) {
-        /**
-         * TODO: Find the car by ID from the `repository` if it exists.
-         *   If it does not exist, throw a CarNotFoundException
-         */
-
-
-        /**
-         * TODO: Delete the car from the repository.
-         */
-
-
+        if (id != null) {
+            repository.findById(id)
+                    .map(car -> {
+                        if (car.getId() != null) {
+                            repository.deleteById(id);
+                        }
+                        return true;
+                    }).orElseThrow(CarNotFoundException::new);
+        }
     }
 }
